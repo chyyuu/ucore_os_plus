9e614defecbe6dc3ad31101ac20cffcf43d7385b
gnocuil <gnocuil@gmail.com>
2012-11-09 18:00:21 +0800

    created nios2_wireless branch

--- ucore/program_user.sh
+++ ucore/program_user.sh
@@ -1,2 +1,3 @@
 bin2flash --input="obj/sfs.img" --output="obj/sfs.flash" --location=0x100000 --verbose 
-nios2-flash-programmer obj/sfs.flash --cable=1 --base=0xc0000000  --program --verbose -g
+nios2-flash-programmer obj/sfs.flash --cable=1 --base=0xc0000000  --program --verbose -g --cable=1
+nios2-flash-programmer obj/sfs.flash --cable=1 --base=0xc0000000  --program --verbose -g --cable=2
