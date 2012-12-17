65544aee61de04fda1152b7e8cf8b94126d69a1b
gnocuil <gnocuil@gmail.com>
2012-12-17 22:02:32 +0800

    removed -g option in nios2 CFLAGS

--- ucore/src/kern-ucore/arch/nios2/include.mk
+++ ucore/src/kern-ucore/arch/nios2/include.mk
@@ -1,5 +1,5 @@
 ARCH_INLUCDES:=debug mm driver init libs sync process glue-ucore glue-ucore/libs syscall
-ARCH_CFLAGS := -g -EL -mhw-div -mhw-mul -mno-hw-mulx -DSYSTEM_BUS_WIDTH=32 \
+ARCH_CFLAGS := -EL -mhw-div -mhw-mul -mno-hw-mulx -DSYSTEM_BUS_WIDTH=32 \
                -pipe -DALT_NO_INSTRUCTION_EMULATION -Wno-traditional -G 0 \
                -fomit-frame-pointer -Wall -O2
 ARCH_LDFLAGS := -m    nios2elf
--- ucore/src/libs-user-ucore/arch/nios2/include.mk
+++ ucore/src/libs-user-ucore/arch/nios2/include.mk
@@ -1,4 +1,4 @@
-ARCH_CFLAGS := -g -EL -mhw-div -mhw-mul -mno-hw-mulx -DSYSTEM_BUS_WIDTH=32 \
+ARCH_CFLAGS := -EL -mhw-div -mhw-mul -mno-hw-mulx -DSYSTEM_BUS_WIDTH=32 \
                -pipe -DALT_NO_INSTRUCTION_EMULATION -Wno-traditional -G 0 \
                -fomit-frame-pointer -Wall -O2
 ARCH_LDFLAGS := -m    nios2elf
