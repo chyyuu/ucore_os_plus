7c4041e840d32a841f604b7d444373493fb0f3b1
gnocuil <gnocuil@gmail.com>
2012-12-17 20:58:20 +0800

    fixed program shell script for NIOS2

--- ucore/program.sh
+++ ucore/program.sh
@@ -1,2 +1,3 @@
-nios2-flash-programmer obj/kernel.flash --cable=1 --base=0xc0000000  --program --verbose -g
+nios2-flash-programmer obj/kernel.flash --cable=1 --base=0xc0000000  --program --verbose -g --cable=1
+nios2-flash-programmer obj/kernel.flash --cable=1 --base=0xc0000000  --program --verbose -g --cable=2
 
