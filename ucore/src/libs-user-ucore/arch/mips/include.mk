ae89a9249ab4c3f5dcda75fae29b56864113cf4f
GNOCUIL <gnocuil@gnocuil-System-Product-Name.(none)>
2012-12-28 17:40:40 +0800

    Modified makefile of MIPS; ADD div/udiv support for THU_MIPS

--- ucore/src/libs-user-ucore/arch/mips/include.mk
+++ ucore/src/libs-user-ucore/arch/mips/include.mk
@@ -1,4 +1,4 @@
-ARCH_CFLAGS := -mips1 -fno-builtin -nostdlib  -nostdinc -g  -EL -G0 -fno-delayed-branch -Wall -O0 -DMACH_QEMU
+ARCH_CFLAGS := -mips1 -g  -EL -G0 -fno-delayed-branch -Wall -O0 -DMACH_QEMU
 ARCH_LDFLAGS := 
-ARCH_OBJS := syscall.o initcode.o intr.o clone.o
+ARCH_OBJS := syscall.o initcode.o intr.o clone.o udivmod.o udivmodsi4.o divmod.o
 ARCH_INITCODE_OBJ := initcode.o
