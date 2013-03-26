31a9ec655d93025c7899846d9b9319ee6041723d
Junjie Mao <eternal.n08@gmail.com>
2013-03-26 11:41:21 +0800

    Add support for autotesting amd64.
    Signed-off-by: Junjie Mao <eternal.n08@gmail.com>

--- autobuild.sh
+++ autobuild.sh
@@ -7,6 +7,7 @@ cd ucore
 # Remove previous builds first
 rm -rf $BUILD_DIR_I386
 rm -rf $BUILD_DIR_ARM
+rm -rf $BUILD_DIR_AMD64
 
 # Build uCore for i386
 make O=$BUILD_DIR_I386 ARCH=i386 defconfig > /dev/null && \
@@ -28,6 +29,15 @@ UCORE_TEST=xx make O=$BUILD_DIR_ARM sfsimg > /dev/null && \
 make O=$BUILD_DIR_ARM kernel > /dev/null
 if [ $? -ne 0 ]; then
     echo "build uCore for arm failed!"
+    exit 1
+fi
+
+# Build uCore for amd64
+make O=$BUILD_DIR_AMD64 ARCH=amd64 defconfig > /dev/null && \
+make O=$BUILD_DIR_AMD64 kernel > /dev/null && \
+UCORE_TEST=xx make O=$BUILD_DIR_AMD64 sfsimg > /dev/null
+if [ $? -ne 0 ]; then
+    echo "build uCore for amd64 failed!"
     exit 1
 fi
 
--- autotest.sh
+++ autotest.sh
@@ -10,11 +10,17 @@ cd ucore
 # Run tests on arm silently
 ./uCore_test -t 45 -w 15 -d $BUILD_DIR_ARM -r -s all > /dev/null 2>&1
 
+# Run tests on amd64 silently
+./uCore_test -d $BUILD_DIR_AMD64 -t 20 -r -s ucore > /dev/null 2>&1
+
 # Dump results to stdout
 echo "==================== i386 ===================="
 cat $BUILD_DIR_I386/test-result.latest/summary
 echo ""
 echo "==================== arm  ===================="
 cat $BUILD_DIR_ARM/test-result.latest/summary
+echo ""
+echo "=================== amd64 ===================="
+cat $BUILD_DIR_AMD64/test-result.latest/summary
 
 popd > /dev/null
