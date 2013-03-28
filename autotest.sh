68ef0dd3a6b71ccd4101c828ae67803206de0e13
Junjie Mao <eternal.n08@gmail.com>
2013-03-28 08:22:27 +0800

    Run test cases from different architectures in parallel
    Signed-off-by: Junjie Mao <eternal.n08@gmail.com>

--- autotest.sh
+++ autotest.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+NR_CPUS=`grep processor /proc/cpuinfo | wc -l`
+
 pushd `dirname $0` > /dev/null
 source autotest.config
 cd ucore
 
-# Run tests on i386 silently
-./uCore_test -d $BUILD_DIR_I386 -t 20 -r -s ucore > /dev/null 2>&1
+if [ $NR_CPUS -ge 4 ]; then
+    ./uCore_test -d $BUILD_DIR_I386 -t 20 -r -s ucore > /dev/null 2>&1 &
+    PIDS[0]=$!
+    ./uCore_test -t 45 -w 15 -d $BUILD_DIR_ARM -r -s all > /dev/null 2>&1 &
+    PIDS[1]=$!
+    ./uCore_test -d $BUILD_DIR_AMD64 -t 20 -r -s ucore > /dev/null 2>&1 &
+    PIDS[2]=$!
 
-# Run tests on arm silently
-./uCore_test -t 45 -w 15 -d $BUILD_DIR_ARM -r -s all > /dev/null 2>&1
-
-# Run tests on amd64 silently
-./uCore_test -d $BUILD_DIR_AMD64 -t 20 -r -s ucore > /dev/null 2>&1
+    for i in {0..2}; do
+	wait ${PIDS[i]}
+    done
+else
+    # Resource limited. Run the tests one by one
+    ./uCore_test -d $BUILD_DIR_I386 -t 20 -r -s ucore > /dev/null 2>&1
+    ./uCore_test -t 45 -w 15 -d $BUILD_DIR_ARM -r -s all > /dev/null 2>&1
+    ./uCore_test -d $BUILD_DIR_AMD64 -t 20 -r -s ucore > /dev/null 2>&1
+fi
 
 # Dump results to stdout
 echo "==================== i386 ===================="
