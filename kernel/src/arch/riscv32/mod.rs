f7b9cdf8a0c9ce77dc97032c3df96be8bb0a515e
chyyuu <yuchen@mail.tsinghua.edu.cn>
2018-11-13 15:14:38 +0800

    clean code. the meaning of boot/boot.asm???

--- kernel/src/arch/riscv32/mod.rs
+++ kernel/src/arch/riscv32/mod.rs
@@ -5,6 +5,6 @@ pub mod io;
 pub mod compiler_rt;
 
 #[cfg(feature = "no_bbl")]
+//without boot.asm, qemu can not run correctly???
 global_asm!(include_str!("boot/boot.asm"));
 global_asm!(include_str!("boot/entry.asm"));
-//global_asm!(include_str!("boot/trap.asm"));
